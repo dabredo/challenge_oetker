ffd7ad97caa75852ca0747f8bf316f9d7c0d3533
David <da.breva@gmail.com>
2020-10-06 19:44:36 +0200

    Handle exceptions

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+           KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        $response = new JsonResponse([ 'error' => 'Internal error' ], 500);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response = new JsonResponse(
+                 [ 'error' => $exception->getMessage()],
+                 $exception->getStatusCode()
+            );
+        }
+
+        $event->setResponse($response);
+    }
+}
